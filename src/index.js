0d770fe5ee2c0f2c9fb46375794915b9ea17adbd
Weslei Luiz de Paula Pinto <weslei.luiz@softwillians.com.br>
2020-03-29 16:19:15 -0300

    Validação das requisições com celebrate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+require('dotenv').config();
 const routes = require('./routes');
 const cors = require('cors');
+const { errors } = require('celebrate');
 
 const port = process.env.PORT || 3333
 const app = express();
@@ -9,6 +11,6 @@ app.use(cors());
 // define tipo de request esperado para aplicação
 app.use(express.json());
 app.use(routes);
-
+app.use(errors())
 
 app.listen(port)
--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,58 @@ const serviceOng = require('./services/OngService');
 const serviceCaso = require('./services/CasoService');
 const serviceSession = require('./services/SessionService');
 const routes = express.Router();
+const { celebrate, Segments, Joi } = require('celebrate');
 
-routes.post('/ongs', serviceOng.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        nome: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        cidade: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), serviceOng.create);
 routes.get('/ongs', serviceOng.getAll);
-routes.get('/ongs/:id/casos', serviceOng.getByOng);
+routes.get('/ongs/:id/casos', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().required()
+    })
+}), serviceOng.getByOng);
 
 routes.get('/', (request, response) => {
     return response.status(200);
 });
 
-routes.post('/casos', serviceCaso.create);
-routes.get('/casos', serviceCaso.getAll);
-routes.delete('/casos/:id', serviceCaso.delete);
+routes.post('/casos', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+        titulo: Joi.string().required(),
+        descricao: Joi.string().required().max(255),
+        valor: Joi.number()
+    })
+}), serviceCaso.create);
 
-routes.post('/session', serviceSession.create);
+routes.get('/casos', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number().min(1)
+    })
+}), serviceCaso.getAll);
+
+routes.delete('/casos/:id', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(),
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), serviceCaso.delete);
+
+routes.post('/session', celebrate({
+    [Segments.BODY]: Joi.object({
+        id: Joi.string().required()
+    })
+}), serviceSession.create);
 
 module.exports = routes;
